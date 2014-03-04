dc5d86b6f26fda3d23075c84f88c8b032c1d50df
danvacam <brusaurio@gmail.com>
2014-03-04 22:53:30 +0100

    layout transparent

---
+++ Conto.Wpf/ContoWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Conto.Wpf
+{
+    /// <summary>
+    /// Logica di interazione per ContoWindow.xaml
+    /// </summary>
+    public partial class ContoWindow : Window
+    {
+        public ContoWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
