ac925d3ddf891074fa04ec89caeeed234102d2a3
danvacam <brusaurio@gmail.com>
2014-03-11 16:34:07 +0100

    wip

--- Conto.Wpf/OptionsUserControl.xaml.cs
+++ Conto.Wpf/OptionsUserControl.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Conto.Wpf
 {
@@ -81,11 +70,6 @@ namespace Conto.Wpf
                 //MaximizeButton.Content = "2";
             }
 
-        }
-
-        private void SettingsButton_Click(object sender, MouseButtonEventArgs e)
-        {
-            
         }
     }
 }
