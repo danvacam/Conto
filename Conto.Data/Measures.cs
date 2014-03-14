72af7343496b429e52ef8a5d2520dd6635a4f755
danvacam <brusaurio@gmail.com>
2014-03-14 18:45:53 +0100

    wip

---
+++ Conto.Data/Measures.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conto.Data
+{
+    public class Measures
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public int Grams { get; set; }
+    }
+}
