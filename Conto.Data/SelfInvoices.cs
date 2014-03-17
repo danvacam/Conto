299cd8a321092500f88fddcff5dadf6fee6da5e0
danvacam <brusaurio@gmail.com>
2014-03-17 16:48:46 +0100

    wip

--- Conto.Data/SelfInvoices.cs
+++ Conto.Data/SelfInvoices.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Conto.Data
 {
@@ -15,5 +15,13 @@ namespace Conto.Data
         public DateTime InvoiceDate { get; set; }
         public decimal InvoiceCost { get; set; }
         public Guid InvoiceGroupId { get; set; }
+    }
+
+    public class SelfInvoicesMaster
+    {
+        public Guid InvoiceGroupId { get; set; }
+        public string MaterialDescription { get; set; }
+        public decimal Quantity { get; set; }
+        public decimal Cost { get; set; }
     }
 }
