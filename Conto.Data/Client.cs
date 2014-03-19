4e3c2b275b8e5ff2eab165f43d34f2a2d1e5f392
danvacam <brusaurio@gmail.com>
2014-03-19 16:48:36 +0100

    wip

---
+++ Conto.Data/Client.cs
@@ -0,0 +1,19 @@
+namespace Conto.Data
+{
+    public class Client
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public string City { get; set; }
+
+        public string PostalCode { get; set; }
+
+        public string FiscalCode { get; set; }
+
+        public string VatCode { get; set; }
+    }
+}
