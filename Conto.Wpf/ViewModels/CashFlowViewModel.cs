d35f4aa1bdc16d4abc72adcf029de1a751231080
danvacam <brusaurio@gmail.com>
2014-03-20 17:41:55 +0100

    wip

--- Conto.Wpf/ViewModels/CashFlowViewModel.cs
+++ Conto.Wpf/ViewModels/CashFlowViewModel.cs
@@ -1,11 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
+using Conto.Data;
 using Conto.Wpf.Resources;
 
 namespace Conto.Wpf.ViewModels
@@ -22,10 +20,21 @@ namespace Conto.Wpf.ViewModels
     public class CashFlowViewModel : INotifyPropertyChanged
     {
 
+        private readonly ContoData _contoData;
+
         public CashFlowViewModel()
         {
+            _contoData = new ContoData();
+
             DepositCommand = new RelayCommand(DepositCommand_Executed);
             CostCommand = new RelayCommand(CostCommand_Executed);
+            FilterGridCommand = new RelayCommand(FilterGridCommand_Executed);
+
+            var lastDate = _contoData.CashFlowGetLastDateTime();
+            SelectedYear = lastDate.Year;
+            SelectedMonth = lastDate.Month;
+
+            CashFlows = new List<CashFlow>(_contoData.CashFlowGetYearMonth(lastDate.Year, lastDate.Month));
 
             DepositDate = DateTime.Now;
             CostDate = DateTime.Now;
@@ -101,6 +110,68 @@ namespace Conto.Wpf.ViewModels
             }
         }
 
+        #region CASH FLOW GRID
+
+        private int? _selectedMonth;
+        public int? SelectedMonth
+        {
+            get { return _selectedMonth; }
+            set
+            {
+                _selectedMonth = value;
+                OnPropertyChanged("SelectedMonth");
+            }
+        }
+
+        private Dictionary<int, string> _months;
+        public Dictionary<int, string> Months
+        {
+            get { return _months; }
+            set
+            {
+                _months = value;
+                OnPropertyChanged("Months");
+            }
+        }
+
+        private int? _selectedYear;
+        public int? SelectedYear
+        {
+            get { return _selectedYear; }
+            set
+            {
+                _selectedYear = value;
+                OnPropertyChanged("SelectedYear");
+            }
+        }
+
+        private Dictionary<int, string> _years;
+        public Dictionary<int, string> Years
+        {
+            get { return _years; }
+            set
+            {
+                _years = value;
+                OnPropertyChanged("Years");
+            }
+        }
+
+        private List<CashFlow> _cashFlows;
+        public List<CashFlow> CashFlows
+        {
+            get
+            {
+                return _cashFlows;
+            }
+            set
+            {
+                _cashFlows = value;
+                OnPropertyChanged("CashFlows");
+            }
+        }
+
+        #endregion
+
         #region Commands
 
         public ICommand DepositCommand { get; set; }
@@ -115,6 +186,16 @@ namespace Conto.Wpf.ViewModels
         public void CostCommand_Executed(object sender)
         {
 
+        }
+
+        public ICommand FilterGridCommand { get; set; }
+
+        public void FilterGridCommand_Executed(object sender)
+        {
+            if (SelectedYear.HasValue && SelectedMonth.HasValue)
+                CashFlows = new List<CashFlow>(_contoData.CashFlowGetYearMonth(SelectedYear.Value, SelectedMonth.Value));
+            else
+                MessageBox.Show("Selezionare anno e mese");
         }
 
         #endregion
