09519779073f70e3e26e97ed4296e1fe533064f1
danvacam <brusaurio@gmail.com>
2014-03-20 17:41:16 +0100

    wip

---
+++ Conto.Data/CashFlow.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conto.Data
+{
+    public class CashFlow
+    {
+        public long Id { get; set; }
+        public decimal Cash { get; set; }
+        public string Description { get; set; }
+        public DateTime FlowDate { get; set; }
+    }
+}
