fc0eb9a5f22ca45f7ea362f9cd15e2ea347978d1
danvacam <brusaurio@gmail.com>
2014-03-26 17:43:51 +0100

    Update Material.cs

--- Conto.Data/Material.cs
+++ Conto.Data/Material.cs
@@ -12,5 +12,6 @@ namespace Conto.Data
         public string Description { get; set; }
         public decimal? Price { get; set; }
         public int MeasureId { get; set; }
+        public string MeasureDescription { get; set; }
     }
 }
