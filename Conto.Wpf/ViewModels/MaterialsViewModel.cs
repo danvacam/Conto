542ffa0aa7a580c3a06c1caf7c11398020a4d0c2
danvacam <brusaurio@gmail.com>
2014-03-26 17:42:25 +0100

    Update MaterialsViewModel.cs

--- Conto.Wpf/ViewModels/MaterialsViewModel.cs
+++ Conto.Wpf/ViewModels/MaterialsViewModel.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using System.Windows;
 using System.Windows.Input;
 using Conto.Data;
@@ -9,28 +11,27 @@ namespace Conto.Wpf.ViewModels
 {
     public class MaterialsViewModel : INotifyPropertyChanged
     {
+        private const int NumberOfRowsInMaterialsGrid = 10;
         private readonly ContoData _contoData;
         public MaterialsViewModel()
         {
             _contoData = new ContoData();
-            Materials = new List<Material>(_contoData.MaterialsGet());
             Measures = new List<Measures>(_contoData.MeasuresGet());
+
+            _completeList = new List<Material>(_contoData.MaterialsGet());
+            NumberOfPages =  (int) Math.Ceiling((double) _completeList.Count / NumberOfRowsInMaterialsGrid);
+            SetMaterialsList();
+
             AddMaterialCommand = new RelayCommand(AddMaterial_Executed);
         }
 
-        private int _id;
-        public int Id
+        private void SetMaterialsList(int pageIndex = 0)
         {
-            get
-            {
-                return _id;
-            }
-            set
-            {
-                _id = value;
-                OnPropertyChanged("Id");
-            }
+            PageIndex = pageIndex + 1;
+            Materials = _completeList.Skip(pageIndex * NumberOfRowsInMaterialsGrid).Take(NumberOfRowsInMaterialsGrid).ToList();
         }
+
+        #region NEW MATERIAL FORM PROPERTIES
 
         private string _description;
         public string Description
@@ -61,7 +62,6 @@ namespace Conto.Wpf.ViewModels
         }
 
         private int? _selectedMeasure;
-
         public int? SelectedMeasure
         {
             get { return _selectedMeasure; }
@@ -86,6 +86,12 @@ namespace Conto.Wpf.ViewModels
             }
         }
 
+        #endregion
+
+        #region MATERIALS GRID PROPERTIES
+
+        private readonly List<Material> _completeList;
+
         private List<Material> _materials;
         public List<Material> Materials
         {
@@ -99,6 +105,38 @@ namespace Conto.Wpf.ViewModels
                 OnPropertyChanged("Materials");
             }
         }
+
+        private int _pageIndex;
+        public int PageIndex
+        {
+            get
+            {
+                return _pageIndex;
+            }
+            set
+            {
+                _pageIndex = value;
+                OnPropertyChanged("PageIndex");
+            }
+        }
+
+        private int _numberOfPages;
+        public int NumberOfPages
+        {
+            get
+            {
+                return _numberOfPages;
+            }
+            set
+            {
+                _numberOfPages = value;
+                OnPropertyChanged("NumberOfPages");
+            }
+        }
+
+        #endregion
+
+        #region COMMANDS
 
         public ICommand AddMaterialCommand { get; set; }
         public void AddMaterial_Executed(object sender)
@@ -117,7 +155,7 @@ namespace Conto.Wpf.ViewModels
                 }
                 if (!SelectedMeasure.HasValue)
                 {
-                    MessageBox.Show("Nessuna unità di misura selezionata");
+                    MessageBox.Show("Nessuna unitÃ  di misura selezionata");
                     return;
                 }
                 _contoData.MaterialAdd(new Material
@@ -134,6 +172,58 @@ namespace Conto.Wpf.ViewModels
                 AppProperties.FormHaveModifications = false;
             }
         }
+
+        #region PAGING COMMANDS
+
+        private ICommand _firstPageCommand;
+        public ICommand FirstPage
+        {
+            get { return _firstPageCommand ?? (_firstPageCommand = new RelayCommand(First_Page)); }
+        }
+
+        public void First_Page(object sender)
+        {
+            SetMaterialsList();
+        }
+
+        private ICommand _previousPageCommand;
+        public ICommand PreviousPage
+        {
+            get { return _previousPageCommand ?? (_previousPageCommand = new RelayCommand(Previous_Page)); }
+        }
+
+        public void Previous_Page(object sender)
+        {
+            if (PageIndex > 1)
+                SetMaterialsList(PageIndex - 2);
+        }
+
+        private ICommand _nextPageCommand;
+        public ICommand NextPage
+        {
+            get { return _nextPageCommand ?? (_nextPageCommand = new RelayCommand(Next_Page)); }
+        }
+
+        public void Next_Page(object sender)
+        {
+            if(PageIndex < NumberOfPages)
+                SetMaterialsList(PageIndex);
+        }
+
+        private ICommand _lastPageCommand;
+        public ICommand LastPage
+        {
+            get { return _lastPageCommand ?? (_lastPageCommand = new RelayCommand(Last_Page)); }
+        }
+
+        public void Last_Page(object sender)
+        {
+            SetMaterialsList(NumberOfPages - 1);
+        }
+
+        #endregion
+
+        #endregion
 
         public event PropertyChangedEventHandler PropertyChanged;
         private void OnPropertyChanged(string propertyName)
