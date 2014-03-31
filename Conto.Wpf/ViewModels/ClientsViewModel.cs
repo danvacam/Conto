4ea7f2085fb4e4e6cea75ae59dd59dc1c9f52247
danvacam <brusaurio@gmail.com>
2014-03-31 12:14:45 +0200

    wip

--- Conto.Wpf/ViewModels/ClientsViewModel.cs
+++ Conto.Wpf/ViewModels/ClientsViewModel.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
+using System.Windows;
 using System.Windows.Input;
 using Conto.Data;
 using Conto.Wpf.Resources;
@@ -8,15 +11,32 @@ namespace Conto.Wpf.ViewModels
 {
     public class ClientsViewModel : INotifyPropertyChanged
     {
+        private const int NumberOfRowsInClientsGrid = 10;
         private readonly ContoData _contoData;
 
         public ClientsViewModel()
         {
             _contoData = new ContoData();
-            Clients = new List<Client>(_contoData.ClientsGet());
+
+            _completeList = new List<Client>(_contoData.ClientsGet());
+            NumberOfPages = (int)Math.Ceiling((double)_completeList.Count / NumberOfRowsInClientsGrid);
+            SetClientsList();
 
             AddClientCommand = new RelayCommand(AddClientCommand_Executed);
+            RemoveClientCommand = new RelayCommand(RemoveClientCommand_Executed);
+            ModifyClientCommand = new RelayCommand(ModifyClientCommand_Executed);
+            UpdateClientCommand = new RelayCommand(UpdateClientCommand_Executed);
+
+            UpdatePanelVisibility = Visibility.Collapsed;
         }
+
+        private void SetClientsList(int pageIndex = 0)
+        {
+            PageIndex = pageIndex + 1;
+            Clients = _completeList.Skip(pageIndex * NumberOfRowsInClientsGrid).Take(NumberOfRowsInClientsGrid).ToList();
+        }
+
+        #region NEW CLIENT PROPERTIES
 
         private string _name;
         public string Name
@@ -84,6 +104,12 @@ namespace Conto.Wpf.ViewModels
             }
         }
 
+        #endregion
+
+        #region CLIENTS GRID PROPERTIES
+
+        private List<Client> _completeList;
+
         private List<Client> _clients;
         public List<Client> Clients
         {
@@ -98,31 +124,293 @@ namespace Conto.Wpf.ViewModels
             }
         }
 
+        private int _pageIndex;
+        public int PageIndex
+        {
+            get
+            {
+                return _pageIndex;
+            }
+            set
+            {
+                _pageIndex = value;
+                OnPropertyChanged("PageIndex", false);
+            }
+        }
+
+        private int _numberOfPages;
+        public int NumberOfPages
+        {
+            get
+            {
+                return _numberOfPages;
+            }
+            set
+            {
+                _numberOfPages = value;
+                OnPropertyChanged("NumberOfPages", false);
+            }
+        }
+
+        #endregion
+
+        #region UPDATE CLIENTS PROPERTIES
+
+        private long _existingId;
+        public long ExistingId
+        {
+            get
+            {
+                return _existingId;
+            }
+            set
+            {
+                _existingId = value;
+                OnPropertyChanged("ExistingId", false);
+            }
+        }
+
+        private string _existingName;
+        public string ExistingName
+        {
+            get
+            {
+                return _existingName;
+            }
+            set
+            {
+                _existingName = value;
+                OnPropertyChanged("ExistingName", false);
+            }
+        }
+
+        private string _existingAddress;
+        public string ExistingAddress
+        {
+            get
+            {
+                return _existingAddress;
+            }
+            set
+            {
+                _existingAddress = value;
+                OnPropertyChanged("ExistingAddress", false);
+            }
+        }
+
+        private string _existingCity;
+        public string ExistingCity
+        {
+            get
+            {
+                return _existingCity;
+            }
+            set
+            {
+                _existingCity = value;
+                OnPropertyChanged("ExistingCity", false);
+            }
+        }
+
+        private string _existingPostalCode;
+        public string ExistingPostalCode
+        {
+            get
+            {
+                return _existingPostalCode;
+            }
+            set
+            {
+                _existingPostalCode = value;
+                OnPropertyChanged("ExistingPostalCode", false);
+            }
+        }
+
+        private string _existingFiscalCode;
+        public string ExistingFiscalCode
+        {
+            get
+            {
+                return _existingFiscalCode;
+            }
+            set
+            {
+                _existingFiscalCode = value;
+                OnPropertyChanged("ExistingFiscalCode", false);
+            }
+        }
+
+        private string _existingVatCode;
+        public string ExistingVatCode
+        {
+            get
+            {
+                return _existingVatCode;
+            }
+            set
+            {
+                _existingVatCode = value;
+                OnPropertyChanged("ExistingVatCode", false);
+            }
+        }
+
+        
+        private Visibility _updatePanelVisibility;
+        public Visibility UpdatePanelVisibility
+        {
+            get { return _updatePanelVisibility; }
+            set
+            {
+                _updatePanelVisibility = value;
+                OnPropertyChanged("UpdatePanelVisibility", false);
+            }
+        }
+
+        #endregion
+
+        #region COMMANDS
+
         public ICommand AddClientCommand { get; set; }
         public void AddClientCommand_Executed(object sender)
         {
+            if (AppProperties.FormHaveModifications)
+            {
+                _contoData.ClientAdd(new Client
+                {
+                    Name = Name,
+                    Address = Address,
+                    City = City,
+                    PostalCode = PostalCode,
+                    FiscalCode = FiscalCode,
+                    VatCode = VatCode
+                });
 
-            Name = null;
-            Address = null;
-            City = null;
-            PostalCode = null;
-            FiscalCode = null;
-            VatCode = null;
+                Name = string.Empty;
+                Address = string.Empty;
+                City = string.Empty;
+                PostalCode = string.Empty;
+                FiscalCode = string.Empty;
+                VatCode = string.Empty;
 
-            _contoData.ClientAdd(new Client());
+                _completeList = new List<Client>(_contoData.ClientsGet());
+                NumberOfPages = (int) Math.Ceiling((double) _completeList.Count/NumberOfRowsInClientsGrid);
+                SetClientsList();
 
-            Clients = new List<Client>(_contoData.ClientsGet());
-
-            AppProperties.FormHaveModifications = false;
+                AppProperties.FormHaveModifications = false;
+            }
         }
 
+        public ICommand RemoveClientCommand { get; set; }
+        public void RemoveClientCommand_Executed(object sender)
+        {
+
+            if (_contoData.ClientDelete((Client)sender))
+            {
+                _completeList = new List<Client>(_contoData.ClientsGet());
+                NumberOfPages = (int)Math.Ceiling((double)_completeList.Count / NumberOfRowsInClientsGrid);
+                SetClientsList();
+                AppProperties.FormHaveModifications = false;
+            }
+            else
+            {
+                MessageBox.Show("Impossibile eliminare il Cliente perchè in uso nelle fatture!");
+            }
+        }
+
+        public ICommand ModifyClientCommand { get; set; }
+        public void ModifyClientCommand_Executed(object sender)
+        {
+            var client = (Client)sender;
+            ExistingId = client.Id;
+            ExistingName = client.Name;
+            ExistingAddress = client.Address;
+            ExistingCity = client.City;
+            ExistingPostalCode = client.PostalCode;
+            ExistingFiscalCode = client.FiscalCode;
+            ExistingVatCode = client.VatCode;
+
+            UpdatePanelVisibility = Visibility.Visible;
+        }
+
+        public ICommand UpdateClientCommand { get; set; }
+        public void UpdateClientCommand_Executed(object sender)
+        {
+            _contoData.ClientUpdate(new Client
+            {
+                Id = ExistingId,
+                Name = ExistingName,
+                Address = ExistingAddress,
+                City = ExistingCity,
+                PostalCode = ExistingPostalCode,
+                FiscalCode = ExistingFiscalCode,
+                VatCode = ExistingVatCode
+            });
+
+            _completeList = new List<Client>(_contoData.ClientsGet());
+            SetClientsList();
+
+            UpdatePanelVisibility = Visibility.Collapsed;
+        }
+
+        #region PAGING COMMANDS
+
+        private ICommand _firstPageCommand;
+        public ICommand FirstPage
+        {
+            get { return _firstPageCommand ?? (_firstPageCommand = new RelayCommand(First_Page)); }
+        }
+
+        public void First_Page(object sender)
+        {
+            SetClientsList();
+        }
+
+        private ICommand _previousPageCommand;
+        public ICommand PreviousPage
+        {
+            get { return _previousPageCommand ?? (_previousPageCommand = new RelayCommand(Previous_Page)); }
+        }
+
+        public void Previous_Page(object sender)
+        {
+            if (PageIndex > 1)
+                SetClientsList(PageIndex - 2);
+        }
+
+        private ICommand _nextPageCommand;
+        public ICommand NextPage
+        {
+            get { return _nextPageCommand ?? (_nextPageCommand = new RelayCommand(Next_Page)); }
+        }
+
+        public void Next_Page(object sender)
+        {
+            if (PageIndex < NumberOfPages)
+                SetClientsList(PageIndex);
+        }
+
+        private ICommand _lastPageCommand;
+        public ICommand LastPage
+        {
+            get { return _lastPageCommand ?? (_lastPageCommand = new RelayCommand(Last_Page)); }
+        }
+
+        public void Last_Page(object sender)
+        {
+            SetClientsList(NumberOfPages - 1);
+        }
+
+        #endregion
+
+        #endregion
+
         public event PropertyChangedEventHandler PropertyChanged;
-        private void OnPropertyChanged(string propertyName)
+        private void OnPropertyChanged(string propertyName, bool formHaveModifications = true)
         {
             if (PropertyChanged != null)
             {
                 PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
-                AppProperties.FormHaveModifications = true;
+                AppProperties.FormHaveModifications = formHaveModifications;
             }
         }
     }
