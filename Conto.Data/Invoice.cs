23c8c2991aa12b361bb2eab9fcd85d53a61dc278
danvacam <brusaurio@gmail.com>
2014-04-04 08:58:09 +0200

    wip

---
+++ Conto.Data/Invoice.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conto.Data
+{
+    public class Invoice
+    {
+        public long Id { get; set; }
+        public long ClientId { get; set; }
+        public DateTime InvoiceDate { get; set; }
+        public long InvoiceNumber { get; set; }
+        public int InvoiceYear { get; set; }
+        public int MeasureId { get; set; }
+        public IEnumerable<InvoiceContent> InvoiceContentRows { get; set; }
+    }
+
+    public class InvoiceContent
+    {
+        public string Description { get; set; }
+        public IEnumerable<InvoiceMaterialRow> Rows { get; set; }
+    }
+
+    public class InvoiceMaterialRow
+    {
+        public string Description { get; set; }
+        public decimal MaterialQuantity { get; set; }
+        public decimal MaterialPrice { get; set; }
+        public decimal MaterialCost { get; set; }
+    }
+}
