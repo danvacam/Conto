766cf25cfeb0d02ab07f4114061971c96cb36ba9
danvacam <brusaurio@gmail.com>
2014-04-04 08:57:33 +0200

    Update SelfInvoicesMaster.cs

--- Conto.Data/SelfInvoicesMaster.cs
+++ Conto.Data/SelfInvoicesMaster.cs
@@ -5,10 +5,15 @@ namespace Conto.Data
     public class SelfInvoicesMaster
     {
         public Guid InvoiceGroupId { get; set; }
+        public long MaterialId { get; set; }
         public string MaterialDescription { get; set; }
         public decimal Quantity { get; set; }
         public decimal Cost { get; set; }
         public DateTime InvoiceDate { get; set; }
         public long InvoiceCount { get; set; }
+        public int MeasureId { get; set; }
+        public string MeasuresDescription { get; set; }
+        public bool VatExcept { get; set; }
+        public int InvoiceYear { get; set; }
     }
 }
